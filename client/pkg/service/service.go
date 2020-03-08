4f579a6cfd4d5403487e2116939dfeb6ab79d71c
FixerOne <61922355+FixerOne@users.noreply.github.com>
2020-03-08 14:05:10 -0500

    Testing new Framework GO KIT

---
+++ client/pkg/service/service.go
@@ -0,0 +1,7 @@
+package service
+
+// ClientService describes the service.
+type ClientService interface {
+	// Add your methods here
+	// e.x: Foo(ctx context.Context,s string)(rs string, err error)
+}
---
+++ project/pkg/service/service.go
@@ -0,0 +1,7 @@
+package service
+
+// ProjectService describes the service.
+type ProjectService interface {
+	// Add your methods here
+	// e.x: Foo(ctx context.Context,s string)(rs string, err error)
+}
