c32ec41350daf2309ed61628f1b5e1347268e6c2
FixerOne <61922355+FixerOne@users.noreply.github.com>
2020-03-08 14:22:14 -0500

    Add Service employee

---
+++ employee/pkg/service/service.go
@@ -0,0 +1,7 @@
+package service
+
+// EmployeeService describes the service.
+type EmployeeService interface {
+	// Add your methods here
+	// e.x: Foo(ctx context.Context,s string)(rs string, err error)
+}
